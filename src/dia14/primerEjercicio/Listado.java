39ddc42d2e7ecadb914d2d0628d1f8d5f9e6473f
Daniel Luna González <LunaLunae91@gmail.com>
2020-08-14 04:12:52 -0400

    a

---
+++ src/dia14/primerEjercicio/Listado.java
@@ -0,0 +1,10 @@
+package dia14.primerEjercicio;
+
+    public class Listado {
+
+        public static void main(String[] args) throws ClassNotFoundException{
+
+            Class.forName("com.postgres.jdbc.Driver");
+
+        }
+    }
